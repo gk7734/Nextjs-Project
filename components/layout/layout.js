fcfbf9cd603f8db4bc8496ff239276c346ed095b
gk7734 <gamekiller7734@naver.com>
2023-08-04 23:23:29 +0900

    AllEventPage 구현 완료

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,5 +1,13 @@
-function Layout(prps) {
+import {Fragment} from "react";
+import MainHeader from "./main-header";
 
+function Layout(props) {
+    return <Fragment>
+        <MainHeader />
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
 }
 
 export default Layout;
