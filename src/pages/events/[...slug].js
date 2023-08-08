ae0f7bfe278635eee05f0a149965fa9369196333
gk7734 <gamekiller7734@naver.com>
2023-08-08 19:53:28 +0900

    CSR 업데이트 끝

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -50,7 +50,8 @@ function FilteredEventsPage(props) {
         numYear > 2030 ||
         numYear < 2022 ||
         numMonth < 1 ||
-        numMonth > 12
+        numMonth > 12 ||
+        error
     ) {
         return (
             <Fragment>
@@ -95,45 +96,45 @@ function FilteredEventsPage(props) {
     );
 }
 
-export async function getServerSideProps(context) {
-    const {params} = context;
-
-    const filterData = params.slug;
-
-    const filteredYear = filterData[0];
-    const filteredMonth = filterData[1];
-
-    const numYear = +filteredYear
-    const numMonth = +filteredMonth
-
-    if (
-        isNaN(numYear) ||
-        isNaN(numMonth) ||
-        numYear > 2030 ||
-        numYear < 2022 ||
-        numMonth < 1 ||
-        numMonth > 12
-    ) {
-        return {
-            props: {hasError: true}
-        }
-    }
-
-    const filteredEvents = await getFilteredEvents({
-        year: numYear,
-        month: numMonth
-    });
-
-
-    return{
-        props: {
-            events: filteredEvents,
-            date: {
-                year: numYear,
-                month: numMonth
-            }
-        }
-    }
-}
+// export async function getServerSideProps(context) {
+//     const {params} = context;
+//
+//     const filterData = params.slug;
+//
+//     const filteredYear = filterData[0];
+//     const filteredMonth = filterData[1];
+//
+//     const numYear = +filteredYear
+//     const numMonth = +filteredMonth
+//
+//     if (
+//         isNaN(numYear) ||
+//         isNaN(numMonth) ||
+//         numYear > 2030 ||
+//         numYear < 2022 ||
+//         numMonth < 1 ||
+//         numMonth > 12
+//     ) {
+//         return {
+//             props: {hasError: true}
+//         }
+//     }
+//
+//     const filteredEvents = await getFilteredEvents({
+//         year: numYear,
+//         month: numMonth
+//     });
+//
+//
+//     return{
+//         props: {
+//             events: filteredEvents,
+//             date: {
+//                 year: numYear,
+//                 month: numMonth
+//             }
+//         }
+//     }
+// }
 
 export default FilteredEventsPage;
