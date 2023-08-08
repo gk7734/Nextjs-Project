827eeff3b70a62cfc2025fd54ce609b835f83e48
gk7734 <gamekiller7734@naver.com>
2023-08-08 18:56:31 +0900

    slug 페이지 CSR로 렌더링

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,18 +1,39 @@
 import { useRouter } from "next/router";
-import {flatten} from "next/dist/shared/lib/flatten";
-import {getFilteredEvents} from "../../../dummy-data";
+import useSWR from "swr";
+import {getAllEvents, getFilteredEvents} from "../../../helpers/api-util";
 import EventList from "../../../components/events/event-list";
 import ResultsTitle from "../../../components/events/results-title";
-import {Fragment} from "react";
+import {Fragment, useEffect, useState} from "react";
 import Button from "../../../components/ui/button";
 import ErrorAlert from "../../../components/ui/error-alert";
+import {router} from "next/client";
 
-function FilteredEventsPage() {
-    const router = useRouter();
+const fetcher = (url) => {
+    return fetch(url).then(res => res.json())
+}
 
+function FilteredEventsPage(props) {
+    const [loadedEvents, setLoadedEvents] = useState()
     const filterData = router.query.slug;
+    const url = "https://nextjs-study-eb27b-default-rtdb.firebaseio.com/events.json"
 
-    if (!filterData) {
+    const {data, error} = useSWR(url, fetcher);
+
+    useEffect(() => {
+        if (data) {
+            const events = [];
+
+            for (const key in data) {
+                events.push({
+                    id: key,
+                    ...data[key]
+                })
+            }
+            setLoadedEvents(events)
+        }
+    }, [data]);
+
+    if (!loadedEvents) {
         return <p className="center">Loading...</p>
     }
 
@@ -21,6 +42,7 @@ function FilteredEventsPage() {
 
     const numYear = +filteredYear
     const numMonth = +filteredMonth
+
 
     if (
         isNaN(numYear) ||
@@ -42,9 +64,12 @@ function FilteredEventsPage() {
         );
     }
 
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth
+    const filteredEvents = loadedEvents.filter((event) => {
+        const eventDate = new Date(event.date);
+        return (
+            eventDate.getFullYear() === numYear &&
+            eventDate.getMonth() === numMonth - 1
+        );
     });
 
     if (!filteredEvents || filteredEvents.length === 0) {
@@ -68,6 +93,47 @@ function FilteredEventsPage() {
             <EventList items={filteredEvents}/>
         </Fragment>
     );
+}
+
+export async function getServerSideProps(context) {
+    const {params} = context;
+
+    const filterData = params.slug;
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2022 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return {
+            props: {hasError: true}
+        }
+    }
+
+    const filteredEvents = await getFilteredEvents({
+        year: numYear,
+        month: numMonth
+    });
+
+
+    return{
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth
+            }
+        }
+    }
 }
 
 export default FilteredEventsPage;
--- src/pages/events/[eventId].js
+++ src/pages/events/[eventId].js
@@ -1,22 +1,18 @@
-import { useRouter } from "next/router";
-import {getEventById} from "../../../dummy-data";
 import {Fragment} from "react";
+import {getEventById, getFeaturedEvents} from "../../../helpers/api-util";
 import EventSummary from "../../../components/event-detail/event-summary";
 import EventLogistics from "../../../components/event-detail/event-logistics";
 import EventContent from "../../../components/event-detail/event-content";
 import ErrorAlert from "../../../components/ui/error-alert";
 
-function EventDetailPage() {
-    const router = useRouter();
-
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId)
+function EventDetailPage(props) {
+    const event = props.events;
 
     if (!event) {
         return (
-            <ErrorAlert>
-                <p>No event found</p>
-            </ErrorAlert>
+            <div className="center">
+                <p>Loading...</p>
+            </div>
         );
     }
 
@@ -35,4 +31,29 @@ function EventDetailPage() {
     );
 }
 
+
+
 export default EventDetailPage;
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+
+    const event =  await getEventById(eventId);
+    return {
+        props: {
+            events: event
+        },
+        revalidate: 30
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getFeaturedEvents()
+
+    const paths = events.map((event) => ({params: {eventId: event.id}}))
+
+    return {
+        paths: paths,
+        fallback: true
+    }
+}
